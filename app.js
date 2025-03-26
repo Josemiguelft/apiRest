d2fa8c1682ce9c4ce224c2401e7ad5ba31f262f9
Jose Figueroa <josemiguelft1809@gmail.com>
2025-03-26 14:31:24 -0700

    Se realizo una correccion

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import router from './router/index.js';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import router from "./router/index.js";
 
 // Constantes
 const __filename = fileURLToPath(import.meta.url);
@@ -11,19 +11,33 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // Configuración de la aplicación
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Corse
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  // Manejar preflight requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 // Rutas
-app.use('/', router);
+app.use("/", router);
 
 // Inicialización del servidor
 const puerto = 8080;
 app.listen(puerto, () => {
-    console.log('Iniciando el servidor');
+  console.log("Iniciando el servidor");
 });
